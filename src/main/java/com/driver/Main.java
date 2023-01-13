dd8b7943674cdfbc3abe022a0172393e4c8e7185
krunalSolanki1 <krunalsolucky121@gmail.com>
2023-01-13 14:32:13 +0530

    DD

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println(B.meth());
+    }
+    static class A {
+        static String meth (){
+            return "Invoking method from class A";
+        }
+    }
+    static class B extends  A {
+        static String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
 }
